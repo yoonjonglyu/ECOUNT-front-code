65b8b25c652b530144f65cec22c2bb65c7b7245f
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-08 00:34:51 +0900

    feat: MAIN PAGE CUSTOM COMPONENTS - SEARCH INPUT DEFAULT

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,58 @@ Main.saveDom("infoInput", $('.info-form .data-box')); // set input box
  *      infoInput
  * }  = Main.DOM;
  */
+/**
+ * @description 검색 관련 인풋 컴포넌트
+ * @param {String} category 카테고리 명칭
+ * @param {Object} codeInfo {id} 
+ * @param {Object} labelInfo {readAvail}
+ */
+const searchInput = (category, codeInfo, labelInfo) => {
+    const {
+        infoInput
+    } = Main.DOM;
+    const {
+        id
+    } = codeInfo;
+    const {
+        readAvail
+    } = labelInfo
+    
+    /** create */
+    const box = $Create('div');
+    const label = $Create('label');
+    const span = $Create('span');
+    const inputCode = $Create('input');
+    const button = $Create('button');
+    const inputLabel = $Create('input');
+
+    /** set class */
+    box.className = "search-insert";
+    span.className = "category";
+
+    /** set attr */
+    span.innerText = category;
+    inputCode.id = `code${id}`;
+    inputCode.setAttribute("type", "text");
+    inputCode.setAttribute("name", "code");
+    inputCode.setAttribute("placeholder", category);
+    inputCode.setAttribute("readonly", true);
+    button.innerText = "검색";
+    inputLabel.id = `label${id}`;
+    inputLabel.setAttribute("type", "text");
+    if(readAvail){
+        inputLabel.setAttribute("readonly", true);
+    }
+
+    /** dom tree */
+    label.appendChild(span);
+    label.appendChild(inputCode);
+    label.appendChild(button);
+    label.appendChild(inputLabel);
+
+    box.appendChild(label);
+    infoInput.appendChild(box);
+} 
+searchInput('선택1', {id : 1}, {readAvail : true});
+searchInput('선택2', {id : 2}, {readAvail : true});
+searchInput('선택3', {id : 3}, {readAvail : false});
