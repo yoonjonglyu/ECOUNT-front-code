3828dd812bc0ce38779cb6dc90f80140a3341961
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-07 23:16:26 +0900

    feat: BASIC LIB - INDEX JS

---
+++ assets/index.js
@@ -0,0 +1,33 @@
+/**
+ * @description DOM select 간편화
+ * @param {String} target
+ */
+const $ = (target) => {
+    return document.querySelector(target);
+}
+/**
+ * @description DOM selectAll 간편화
+ * @param {String} target
+ */
+const $$ = (target) => {
+    return document.querySelectorAll(target);
+}
+/**
+ * @class JSDOM
+ * @description DOM 셀렉 관련 클래스 Data Object
+ */
+
+class JSDOM {
+    constructor(_dom){
+        this._dom = {}
+    }
+
+    get DOM () {
+        return this._dom;
+    }
+    set DOM ({ key, value }){
+        if(key !== null && value !== null){
+            this._dom[key] = value;
+        }
+    }
+}
